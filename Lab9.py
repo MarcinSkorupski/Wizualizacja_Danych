8222a9964049368805580f44677787284d03e682
MarcinSkorupski <121235978+MarcinSkorupski@users.noreply.github.com>
2023-05-31 14:37:15 +0200

    Update Lab9.py
    Add missing examples

--- Lab9.py
+++ Lab9.py
@@ -28,6 +28,44 @@ import pandas as pd
 # df['Kontynent'] = ['Europa', 'Azja', 'Ameryka Południowa']
 # print(df)
 
+# pojedynczy element serii, odnosimy się do nazwy indeksu
+print(s['c'])
+# możemy również dostać się do wartości serii jak do pola klasy
+print(s.c)
+# pojedynczy elemenet ramki danych, tak jak przy cięciu tablic, oparte na indeksach
+print(df[0:1])
+print("")
+# kolumna po etykiecie
+print(df['Populacja'])
+# pobieranie pojedynczej wartości po indeksie wiersza i kolumny
+print(df.iloc[0, 0])
+# pobieranie wartości po indeksie wiersza i etykiecie kolumny
+print(df.loc[0, "Kraj"])
+print(df.at[0, "Kraj"])
+# podobnie jak wprzypadku serii można odwoływać się do kolumn jak do pól klasy
+# dodatkowo print jest wywoływany jak w pętli dla każdego elementu danej kolumny
+print('kraj: ' + df.Kraj)
+# Pandas posiada również funkcje pozwalające na losowe pobieranie elementów lub
+# w odniesieniu do procentowej wielkości całego zbioru# jeden losowy element
+print(df.sample())
+#n losowych elementów
+print(df.sample(2))
+# ilość elementów procentowo, uwaga na zaokrąglenie
+print(df.sample(frac=0.5))
+# jeżeli potrzeba nam więcej próbek niż znajduje się w zbiorze i dopuszczamy duplikaty
+# to możemy użyć parametru replace, który będzie losował z powtórzeniami
+print(df.sample(n=10, replace=True))
+# zamiast wyświetlać całą kolekcje możemy wyświetlić określoną ilość elementów od początku kolekcji
+# lub od jej końca
+print(df.head())
+print(df.head(2))
+print(df.tail(1))
+# Pandas jest też w stanie wyświetlić statystykę dla wartości, które dana kolekcja zawiera, o ile są jakieś kolumny
+# z danymi numerycznymi
+print(df.describe())
+# transpozycja to zmienna T kolekcji, podobnie jak w Numpy
+print(df.T)
+
 #Zad 1
 imiona_xlsx = pd.ExcelFile('imiona.xlsx')
 data_frame = pd.read_excel(imiona_xlsx, header=0)
